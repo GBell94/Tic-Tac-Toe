46efc16222b128d737a666cbc0e9c9aa1b4b624c
GBell94 <gmb1294@gmail.com>
2022-03-28 17:36:09 +0000

    Add board and function to print out empty game board to terminal

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+Set up game board and set current player to 'X', which will be the user
+User will have first turn
+"""
+board = ['-' for x in range(9)]
+
+current_player = 'X'
+
+def print_board(board):
+    print("""
+    {0} | {1} | {2}
+    ---------
+    {3} | {4} | {5}
+    ---------
+    {6} | {7} | {8}""".format(board[0],board[1],board[2],board[3],board[4],board[5],
+    board[6],board[7],board[8]))
