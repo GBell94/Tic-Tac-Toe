9cfa75644a5ef816da3d34458a33fd85d96073fa
GBell94 <gmb1294@gmail.com>
2022-03-28 17:45:06 +0000

    Add game welcome text to be printed and function to print board positions

--- run.py
+++ run.py
@@ -16,3 +16,21 @@ def print_board(board):
     ---------
     {6} | {7} | {8}""".format(board[0],board[1],board[2],board[3],board[4],board[5],
     board[6],board[7],board[8]))
+
+
+def welcome():
+    board = [x for x in range(1, 10)]
+    print("""
+    {0} | {1} | {2}
+    ---------
+    {3} | {4} | {5}
+    ---------
+    {6} | {7} | {8}""".format(board[0],board[1],board[2],board[3],board[4],board[5],
+    board[6],board[7],board[8]))
+    print()
+
+
+
+
+print('Welcome to Tic Tac Toe! \nYou are X, the computer is O. \nYou go first - choose your move by selecting your space on the board.\nGet a straight line of Xs (row, column, diagonal) to win.')
+welcome()
