5b8bc3e48bd597051d7ff777c5c72f9431eae2ef
GBell94 <gmb1294@gmail.com>
2022-03-28 18:51:54 +0000

    Add function to obtain possible computer moves

--- run.py
+++ run.py
@@ -136,6 +136,50 @@ def play_again():
         return False  
 
 
+def winner(board, letter):
+    return ((board[0] == board[1] == board[2] == letter) or
+        (board[3] == board[4] == board[5] == letter) or
+        (board[6] == board[7] == board[8] == letter) or
+        (board[0] == board[3] == board[6] == letter) or
+        (board[1] == board[4] == board[7] == letter) or
+        (board[2] == board[5] == board[8] == letter) or
+        (board[0] == board[4] == board[8] == letter) or
+        (board[2] == board[1] == board[6] == letter))  
+
+
+def computer(board):
+    possible_moves = []
+    for i in range(0,9):
+        if board[i] == '-':
+            possible_moves.append(i)
+
+    move = 0
+
+    for letter in ['X', 'O']:
+        for i in possible_moves:
+            copy_board = board[:]
+            copy_board[i] = letter
+            if winner(copy_board, letter):
+                return i
+
+    if 4 in possible_moves:
+        return 4
+
+    corners = []  
+    for i in possible_moves:
+        if i in [0, 2, 6, 8]:
+            corners.append(i)
+    if len(corners) > 0:
+        return random.choice(corners)
+
+    sides = []
+    for i in possible_moves:
+        if i in [1, 3, 5, 7]:
+            sides.append(i)
+    if len(sides) > 0:
+        return random.choice(sides)
+
+
 print('Welcome to Tic Tac Toe! \nYou are X, the computer is O. \nYou go first - choose your move by selecting your space on the board.\nGet a straight line of Xs (row, column, diagonal) to win.')
 welcome()
 
